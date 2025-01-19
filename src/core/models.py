d0bdfd6e66b1ef04a6532818e16df6cb017dee57
njzwj <njzwj2007@126.com>
2025-01-19 16:51:52 +0800

    Add models.

---
+++ src/core/models.py
@@ -0,0 +1,25 @@
+from langchain_openai import AzureChatOpenAI
+from transformers import pipeline
+
+from .config import get_config
+
+config = get_config()
+
+def get_azure_chat_openai(**kwargs):
+    model = AzureChatOpenAI(
+        azure_endpoint=config.azure_openai_endpoint,
+        azure_deployment=config.azure_openai_deployment,
+        openai_api_version=config.azure_openai_api_version,
+        temperature=0,
+        **kwargs,
+    )
+    return model
+
+def get_hf_whisper_large_v3_turbo(**kwargs):
+    model = pipeline(
+        "automatic-speech-recognition",
+        model="openai/whisper-large-v3-turbo",
+        chunk_length_s=30,
+        **kwargs,
+    )
+    return model
