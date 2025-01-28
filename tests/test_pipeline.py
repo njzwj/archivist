c9fa8ab313ca074ab4eb3e9c845b9c2e81d26b96
njzwj <njzwj2007@126.com>
2025-01-28 22:46:17 +0800

    Add orchestrator call UT.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -26,3 +26,21 @@ def test_pipeline_with_loop():
     orchestrator.register(p2)
     with pytest.raises(ValueError):
         orchestrator.register(p3)
+
+def test_pipeline_call_generate():
+    def p1(inputs: dict, **kwargs) -> dict:
+        return {**inputs, "j": inputs["i"] + 1}
+    
+    def p2(inputs: dict, **kwargs) -> dict:
+        return {**inputs, "k": inputs["j"] + 1}
+
+    p1 = Pipeline("p1", ["i"], ["j"], p1)
+    p2 = Pipeline("p2", ["j"], ["k"], p2)
+    orchestrator = PipelineOrchestrator()
+    orchestrator.register(p1)
+    orchestrator.register(p2)
+    inputs = {"i": 1}
+    result = orchestrator.process("p1", inputs)
+    assert result == {"i": 1, "j": 2}
+    result = orchestrator.process("p2", inputs)
+    assert result == {"i": 1, "j": 2, "k": 3}
