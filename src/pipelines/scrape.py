d126cd5f648606166bd0e65556d5867c18f5b0b9
njzwj <njzwj2007@126.com>
2025-02-27 22:42:38 +0800

    Make sure configs only called when running.

--- src/pipelines/scrape.py
+++ src/pipelines/scrape.py
@@ -8,7 +8,6 @@ import requests
 from src.core.models import get_chat_model
 from src.core.pipeline import Pipeline
 
-
 ua = UserAgent()
 text_maker = html2text.HTML2Text()
 text_maker.ignore_links = True
@@ -17,36 +16,36 @@ text_maker.ignore_emphasis = True
 text_maker.ignore_tables = True
 text_maker.ignore_anchors = True
 
-model = get_chat_model()
-
-
 def c(inputs):
     return inputs.content
 
+def get_extract_keys_chain():
+    model = get_chat_model()
 
-extract_keys_chain = (
-    PromptTemplate.from_template(
-        """Here is a web-scrapped content. Please extract the keys from the content.
-        content:
-        ```
-        {content}
-        ```
-        Extract the following keys:
-        - published_at (the published date time, e.g., 2022-01-31 18:23:45)
-        - author
-        Ouput format:
-        ```json
-        {{"published_at": "2021-01-01","author": "John Doe"}}
-        ```
-        But replace the values with the actual values from the content.
-        The original content may not contain exactly the same name for the keys. Decide the best value to use for each key.
-        If the key is not found, leave it empty.
-        Do not include any other keys.
-        """
+    extract_keys_chain = (
+        PromptTemplate.from_template(
+            """Here is a web-scrapped content. Please extract the keys from the content.
+            content:
+            ```
+            {content}
+            ```
+            Extract the following keys:
+            - published_at (the published date time, e.g., 2022-01-31 18:23:45)
+            - author
+            Ouput format:
+            ```json
+            {{"published_at": "2021-01-01","author": "John Doe"}}
+            ```
+            But replace the values with the actual values from the content.
+            The original content may not contain exactly the same name for the keys. Decide the best value to use for each key.
+            If the key is not found, leave it empty.
+            Do not include any other keys.
+            """
+        )
+        | model
+        | c
     )
-    | model
-    | c
-)
+    return extract_keys_chain
 
 
 def extract_header_metadata(soup: BeautifulSoup) -> dict:
@@ -64,6 +63,7 @@ def extract_header_metadata(soup: BeautifulSoup) -> dict:
 
 def extract_keys(obj: dict) -> dict:
     content = json.dumps(obj, ensure_ascii=False)
+    extract_keys_chain = get_extract_keys_chain()
     keys = extract_keys_chain.invoke({"content": content})
     keys = keys.replace("```json", "").replace("```", "").strip()
     return json.loads(keys)
--- src/pipelines/tag.py
+++ src/pipelines/tag.py
@@ -7,7 +7,6 @@ from src.core.models import get_chat_model
 from src.utils import get_config
 
 
-model = get_chat_model()
 config = get_config()
 
 
@@ -15,55 +14,59 @@ def c(inputs):
     return inputs.content
 
 
-summarize_chain = (
-    PromptTemplate.from_template(
+def get_summarize_chain():
+    model = get_chat_model()
+    summarize_chain = (
+        PromptTemplate.from_template(
+            """
+        ```
+        {object}
+        ```
+        Above is a piece of information pulled and processed from the internet.
+        Summarize the content in a few sentences. The topic and key points should be clear. No more than 200 words.
+        Write directly below this line without any additional explanation.
         """
-    ```
-    {object}
-    ```
-    Above is a piece of information pulled and processed from the internet.
-    Summarize the content in a few sentences. The topic and key points should be clear. No more than 200 words.
-    Write directly below this line without any additional explanation.
-    """
+        )
+        | model
+        | c
     )
-    | model
-    | c
-)
+    return summarize_chain
 
+def get_tagging_chain():
+    model = get_chat_model()
+    tagging_chain = (
+        PromptTemplate.from_template(
+            """
+        Content:
+        ```
+        {inputs}
+        ```
+        **Task:** Tag the provided content.
+        Output the tags in a specific format, for example:
+        
+        Input is about a new product launch in E3. Avaiable tags are: [economy,technology,history]
+        Economy is the best tag, the other two are not relevant. So, output will be:
+        1,0,0
 
-tagging_chain = (
-    PromptTemplate.from_template(
+        Another example. If input is about modern word history and economy. Available tags are: [economy,technology,history]
+        Economy and history are relevant, technology is not. So, output will be:
+        1,0,1
+
+        A bad example. If input is about US economy in a decade. Available tags are: [economy,technology,history]
+        A bad output will be:
+        1,0,0,1
+        Because there are only 3 tags available, so the output should be 3 digits.
+        1,0,0 is the correct output.
+
+        So the output should be comma separated 0s and 1s only. And MUST be in the same order and same number as the available tags.
+
+        because the output will be processed by splitting the string by comma, output direct after this line of prompt, without any additional explanation. Available tags are: [{tags}]. You can choose one or multiple tags. Make sure to output in the correct format.
         """
-    Content:
-    ```
-    {inputs}
-    ```
-    **Task:** Tag the provided content.
-    Output the tags in a specific format, for example:
-    
-    Input is about a new product launch in E3. Avaiable tags are: [economy,technology,history]
-    Economy is the best tag, the other two are not relevant. So, output will be:
-    1,0,0
-
-    Another example. If input is about modern word history and economy. Available tags are: [economy,technology,history]
-    Economy and history are relevant, technology is not. So, output will be:
-    1,0,1
-
-    A bad example. If input is about US economy in a decade. Available tags are: [economy,technology,history]
-    A bad output will be:
-    1,0,0,1
-    Because there are only 3 tags available, so the output should be 3 digits.
-    1,0,0 is the correct output.
-
-    So the output should be comma separated 0s and 1s only. And MUST be in the same order and same number as the available tags.
-
-    because the output will be processed by splitting the string by comma, output direct after this line of prompt, without any additional explanation. Available tags are: [{tags}]. You can choose one or multiple tags. Make sure to output in the correct format.
-    """
+        )
+        | model
+        | c
     )
-    | model
-    | c
-)
-
+    return tagging_chain
 
 def tag_content(inputs: dict, **kwargs) -> dict:
     if "tags" in inputs.keys():
@@ -76,6 +79,9 @@ def tag_content(inputs: dict, **kwargs) -> dict:
         )
     available_tags = kwargs["tags"]
     obj = json.dumps(inputs, ensure_ascii=False)
+
+    summarize_chain = get_summarize_chain()
+    tagging_chain = get_tagging_chain()
     summary = summarize_chain.invoke({"object": obj})
     tags = tagging_chain.invoke({"inputs": summary, "tags": available_tags})
 
