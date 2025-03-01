769eb65455143814fecad4765769fd525449838c
njzwj <njzwj2007@126.com>
2025-03-01 16:24:17 +0800

    Remove original files.

--- src/cli/__init__.py
+++
@@ -1,4 +0,0 @@
-from .get import get
-from .mpipe import mpipe
-from .serve import serve
-from .init import run_init
--- src/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import post, post_list, post_list_tag, post_list_author
-
-urlpatterns = [
-    path("", post_list, name="all_posts"),
-    path("tag/<str:tag>/", post_list_tag, name="posts_by_tag"),
-    path("author/<str:author>/", post_list_author, name="posts_by_author"),
-    path("post/<str:slug>/", post, name="post"),
-]
